4547fbb1dbf47da383cc04f8746cc767a5138498
Rakesh Patel <rpatel@onehealth.com>
2014-02-16 10:48:52 -0800

    remove unnecessary closure

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,38 +1,36 @@
-(function () {
-  require('angular/angular');
-  require('angular-resource/angular-resource');
-  require('angular-route/angular-route');
+require('angular/angular');
+require('angular-resource/angular-resource');
+require('angular-route/angular-route');
 
-  var config = require('./config')
-    ,_ = require('lodash');
+var config = require('./config')
+  , _ = require('lodash');
 
-  angular.module(config.appName + '.config',[])
-    .factory('AppConfig',function () {
-      return config;
-    });
-
-  var appDeps = [
-    'ngResource',
-    'ngRoute',
-    config.appName + '.config'
-  ]
-
-  var appModules = {
-    'controllers': require('./controllers/controllers'),
-    'services': require('./services/services'),
-    'filters': require('./filters/filters'),
-    'directives': require('./directives/directives')
-  }
-
-  _.forIn(appModules, function (requireVal, module) {
-    var moduleName = config.appName + '.' + module;
-    requireVal(angular.module(moduleName,[]));
-    appDeps.push(moduleName);
+angular.module(config.appName + '.config', [])
+  .factory('AppConfig', function () {
+    return config;
   });
 
-  var app = angular.module(config.appName,appDeps);
-  require('./routes')(app,config);
+var appDeps = [
+  'ngResource',
+  'ngRoute',
+  config.appName + '.config'
+]
 
-  require('./init')(angular,app,config.appName);
-})();
+var appModules = {
+  'controllers': require('./controllers/controllers'),
+  'services': require('./services/services'),
+  'filters': require('./filters/filters'),
+  'directives': require('./directives/directives')
+}
+
+_.forIn(appModules, function (requireVal, module) {
+  var moduleName = config.appName + '.' + module;
+  requireVal(angular.module(moduleName, []));
+  appDeps.push(moduleName);
+});
+
+var app = angular.module(config.appName, appDeps);
+require('./routes')(app, config);
+
+require('./init')(angular, app, config.appName);
 
